3f250a585bd4772e2d9458d6be1a27bf222cf47d
Roka <bswt01224@gmail.com>
2022-09-02 18:07:46 +0900

    Hello worldを作成

---
+++ src/test/java/com/smilingirl/skillup/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smilingirl.skillup.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
